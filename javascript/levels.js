a7413d672bd700fb4207b60a5cc94b27ced31c06
syhus <marketing@syhus.com.br>
2015-11-02 19:30:15 -0200

    return levels to 1st option

--- javascript/levels.js
+++ javascript/levels.js
@@ -4,26 +4,26 @@ app.factory("levels", function() {
   var levels = [
     {
       minimun_score: 0,
-  		title: "Empreendedor Calouro",
-  		text: "Você começou a trilhar agora o seu caminho. Um hangout exclusivo faria muito bem para seu projeto. Passe no stand da Syhus para combinarmos os detalhes."
+  		title: "Calouro do Marketing Digital",
+  		text: "Você começou a trilhar agora o seu caminho em marketing digital. Continue firme e forte!",
       image: "images/freshman.png"
   	},
-   {
-      minimun_score: 4,
-  	title: "Empreendedor recém-graduado",
--  		text: "Você já aprendeu o essencial e está pronto para seguir seu caminho!",
--      image: "images/graduated.png"
+    {
+      minimun_score: 11,
+  		title: "Recém-graduado em Marketing Digital",
+  		text: "Você já aprendeu o essencial e está pronto para seguir seu caminho!",
+      image: "images/graduated.png"
   	},
     {
-      minimun_score: 7,
-  		title: "Empreendedor Expert",
-  		text: "Você sabe quase tudo sobre gestão. Meia hora de consultoria gratuita te faria muito bem! Passe no stand da Syhus para combinarmos a data!",
+      minimun_score: 16,
+  		title: "Expert em Marketing Digital",
+  		text: "Você já sabe quase tudo de marketing digital.",
       image: "images/expert.png"
   	},
     {
-      minimun_score: 10,
-      title: "Empreendendor Ninja",
-      text: "Você sabe tudo sobre gestão e merece um prêmio! Passe no stand da Syhus para ganhar 1 mês de consultoria gratuita (4 horas)",
+      minimun_score: 20,
+      title: "Ninja do Marketing Digital",
+      text: "Você sabe tudo de marketing digital. Me dá um autógrafo?",
       image: "images/ninja.png"
     }
   ];
